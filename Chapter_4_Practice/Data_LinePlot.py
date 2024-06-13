a7dae4be17aba87a0dcc266ecf75864275ad68ac
TranDinhKhanhNhan <162677112+TranDinhKhanhNhan@users.noreply.github.com>
2024-06-13 20:41:02 +0700

    Add files via upload

---
+++ Chapter_4_Practice/Data_LinePlot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+data = {
+"x" : [i for i in range(1,10)],
+"y" : [i * i for i in range(1,10)]
+}
+
+plt.scatter(
+    data["x"],
+    data["y"],
+    marker = "o"
+)
+plt.grid(True)
+plt.show()
