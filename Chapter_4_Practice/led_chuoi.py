308fc16680a1675b031ffec5c089d61a7e90d9b7
Phạm Tuấn Anh <pta2608@gmail.com>
2024-06-13 20:17:10 +0700

    Update led_chuoi.py
    update led_chuoi.py

--- Chapter_4_Practice/led_chuoi.py
+++ Chapter_4_Practice/led_chuoi.py
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-from gpiozero import LED
 
-led_red = LED(20)
-
-while True:
-    s = input()
-    if s == "on":
-        led_red.on()
-    elif s == "off":
-        led_red.off()
-    else:
-        print("invalid command")
-=======
 from gpiozero import LED
 
 led_red = LED(20)
@@ -28,4 +15,4 @@ while True:
         break
     else:
         print("Unknown command")
->>>>>>> refs/remotes/origin/main
+
