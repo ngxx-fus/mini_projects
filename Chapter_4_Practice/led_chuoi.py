5fcf782d8b2ae8ad5d6cfa3d176121705dcfd6be
Nguyễn Lê Vĩnh Ngọc <74646799+demybug123@users.noreply.github.com>
2024-06-13 20:01:24 +0700

    Add files via upload

---
+++ Chapter_4_Practice/led_chuoi.py
@@ -0,0 +1,16 @@
+from gpiozero import LED
+
+led_red = LED(20)
+
+while True:
+    s = input("Enter command: ")
+    if s in ["on","ON"]:
+        print("Red LED on")
+        led_red.on()
+    elif s in ["off","OFF"]:
+        print("Red LED off")
+        led_red.off()
+    elif s in ["q","quit"]:
+        break
+    else:
+        print("Unknown command")
