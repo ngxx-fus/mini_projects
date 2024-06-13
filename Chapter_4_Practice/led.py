eb84a08a32149d1cc6b918c9dd1a77a79df0949e
ngxx-fus <msnp@outlook.com.vn>
2024-06-13 20:02:30 +0700

    Add, merge sth

--- Chapter_4_Practice/led.py
+++ Chapter_4_Practice/led.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import datetime
 import time
+import random
 
 #set # is GPIO, not # in board
 GPIO.setmode(GPIO.BCM)
@@ -11,19 +12,24 @@ GPIO.setwarnings(False)
 Pin = [ 3, 19, 26]
 
 #set pin is output pin
-GPIO.setup(pin, GPIO.OUT)
+GPIO.setup(Pin, GPIO.OUT)
 
 #list of state
-St = [[ GPIO.HIGH, GPIO.HIGH, GPIO.LOW],
-	[GPIO.LOW, GPIO.HIGH, GPIO.HIGH],
-	[GPIO.HIGH, GPIO.LOW, GPIO.HIGH],
-	[GPIO.HIGH, GPIO.HIGH, GPIO.LOW],
-	[GPIO.HIGH, GPIO.LOW, GPIO.LOW],
-	[GPIO.LOW, GPIO.LOW, GPIO.LOW],
-	[GPIO.HIGH, GPIO.HIGH, GPIO.HIGH],
-	[GPIO.LOW, GPIO.LOW, GPIO.LOW],
-	[GPIO.LOW, GPIO.HIGH, GPIO.LOW],
-	[GPIO.HIGH, GPIO.HIGH, GPIO.HIGH]]
+St = (( GPIO.HIGH, GPIO.HIGH, GPIO.LOW),
+	(GPIO.LOW, GPIO.HIGH, GPIO.HIGH),
+	(GPIO.HIGH, GPIO.LOW, GPIO.HIGH),
+	(GPIO.HIGH, GPIO.HIGH, GPIO.LOW),
+	(GPIO.HIGH, GPIO.LOW, GPIO.LOW),
+	(GPIO.LOW, GPIO.LOW, GPIO.LOW),
+	(GPIO.HIGH, GPIO.HIGH, GPIO.HIGH),
+	(GPIO.LOW, GPIO.LOW, GPIO.LOW),
+	(GPIO.LOW, GPIO.HIGH, GPIO.LOW),
+	(GPIO.HIGH, GPIO.HIGH, GPIO.HIGH))
 
 if __name__ == "__main__":
-	
+	while True:
+		t = random.random()*0.3 + 0.1
+		s = int(random.random()*10)%len(St)
+		print(f"D: {t}, St: {s}") 
+		GPIO.output(Pin, St[s])
+		time.sleep(t)
