f78c8c19481f924a2ee08e3a1c639d34a5f5478c
ngxx-fus <msnp@outlook.com.vn>
2024-06-13 20:53:27 +0700

    dv

---
+++ Chapter_4_Practice/Data_Visualization_fus.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+import random
+
+data = []
+another_data = []
+fig = plt.figure()
+ax = plt.axes(x=(0,30), )
+
+for i in range(100):
+    data.append(int(random.random()*100)%100)
+    another_data.append(int(random.random()*100)%100)
+
+
+
+counts, bins = np.histogram(data)
+plt.stairs(counts, bins)
+plt.hist(bins[:-1], bins, weights=counts)
+
+plt.subplot(1,2,1)
+plt.plot(range(0,100), data)
+
+plt.subplot(1,2,2)
+plt.plot(range(0,100), data)
+
+plt.show()
