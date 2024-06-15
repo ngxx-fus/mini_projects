faa3adb43eda2f7e63040150f8116fef8dcedcfb
ngxx-fus <nguyenphu3204@outlook.com>
2024-06-16 02:08:30 +0700

    Modifying sth :v

--- OLED_32x128/img_extraction.py
+++ OLED_32x128/img_extraction.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 def extract_img(img):
-    print(img.shape)
+    # print(img.shape)
     h, w, c = img.shape
 
     pkt_list = []
@@ -10,7 +10,7 @@ def extract_img(img):
 
     for page in range(4):
         for col in range(w):
-            for row in range(page*8, page*8+8):
+            for row in range(page*8, page*8+7):
                 # print(f"row: {row} col: {col}")
                 gray = img[row, col, 2]/3 + img[row, col, 1]/3 + img[row, col, 0]/3
                 gray = int(gray)
@@ -21,20 +21,20 @@ def extract_img(img):
             pkt_ac  = 0
 
 
-    # print(len(pkt_list))
-    # k = 0
-    # for i in range(16):
-    #     for j in range(32):
-    #         print(f" {pkt_list[k]},", end='')
-    #         k = k+1
-    #     print('\n', end='')
-    #     if (i+1)%4 == 0:
-    #         print('\n', end='')
+#    print(len(pkt_list))
+#    k = 0
+#    for i in range(16):
+#        for j in range(32):
+#            print(f" {pkt_list[k]},", end='')
+#            k = k+1
+#        print('\n', end='')
+#        if (i+1)%4 == 0:
+#            print('\n', end='')
     return pkt_list
 
-def extract_set_img(imgs_path, img_list):
-    set_of_plt_list = [[]]
-    for filename in img_list:
+def extract_set_img(imgs_path, imgs_list):
+    set_of_plt_list = []
+    for filename in imgs_list:
         img = cv2.imread(imgs_path + "/" + filename)
         set_of_plt_list.append(extract_img(img))
     return set_of_plt_list
